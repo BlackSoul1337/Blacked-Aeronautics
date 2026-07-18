4c15680def4c4cbe13b01c88c6ea67108ac99b6e
BlackSoul1337 <104305279+BlackSoul1337@users.noreply.github.com>
2026-07-18 20:23:39 +0500

    fix(pack): use the shared milk tag

--- pack/kubejs/server_scripts/recipeConflictPatch.js
+++ pack/kubejs/server_scripts/recipeConflictPatch.js
@@ -1,3 +1,7 @@
+ServerEvents.tags('item', event => {
+    event.add('c:drinks/milk', 'minecraft:milk_bucket')
+})
+
 ServerEvents.recipes(event => {
     /* Removing Recipes */
     event.remove({  id: 'vanillabackport:cake'  }),
@@ -8,8 +12,8 @@ ServerEvents.recipes(event => {
     event.replaceInput(
         { input: 'minecraft:milk_bucket' }, // Arg 1: the filter
         'minecraft:milk_bucket',            // Arg 2: the item to replace
-        '#c:foods/milk'         // Arg 3: the item to replace it with
+        '#c:drinks/milk'                    // Arg 3: the item to replace it with
     )
-    
+
     /* Adding Recipes */
 })
